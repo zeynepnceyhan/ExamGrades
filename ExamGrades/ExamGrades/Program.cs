53d4f0b00e6a188c9d9c54561f8afcef7d3b16ef
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 14:24:39 +0300

    exam grades(user input)
    internship

---
+++ ExamGrades/ExamGrades/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int midterm, final, makeup, avg;
+            Console.WriteLine("Please enter your midterm exam's grade: ");
+            midterm = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your final exam's grade: ");
+            final = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your makeup exam's grade: ");
+            makeup = Convert.ToInt32(Console.ReadLine());
+
+            avg = (midterm + final + makeup) / 3;
+
+            if (avg >= 50)
+            {
+                Console.WriteLine("CONGRATULATIONS SUCCESSFULLY PASSED THE CLASSES... Your Average: " + avg);
+            }
+            else
+            {
+                Console.WriteLine("SORRY YOU FAILED... Your Average: " + avg);
+            }
+
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
